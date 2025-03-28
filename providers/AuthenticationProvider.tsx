ed309ba4b091fb70c7802aceaae1c1a707d143a3
simo205j <113094975+Simo205j@users.noreply.github.com>
2025-03-28 11:06:14 +0100

    Added settings + logout

--- providers/AuthenticationProvider.tsx
+++ providers/AuthenticationProvider.tsx
@@ -1,19 +1,21 @@
 import { createContext, useCallback, useContext } from "react";
 import { createUserRequest } from "@/apis/registerAPI";
 import { RegisterForm } from "@/app/register";
-import { LoginForm } from "@/app/index"
+import { LoginForm } from "@/app";
 import { loginUserRequest } from "@/apis/loginAPI";
 import { setBearer } from "@/apis/axiosConfig";
+import * as SecureStore from "expo-secure-store";
+import { router } from "expo-router";
 
 type AuthenticationProviderProps = {
   register: (form: RegisterForm) => Promise<string | null>;
   login: (form: LoginForm) => Promise<void>;
-}
+  logout: () => Promise<void>;
+};
 
 const AuthenticationContext = createContext<AuthenticationProviderProps | undefined>(undefined);
 
 const AuthenticationProvider = ({ children }: { children: React.ReactNode }) => {
-
   const register = useCallback(async (form: RegisterForm) => {
     try {
       const res = await createUserRequest(form);
@@ -28,17 +30,26 @@ const AuthenticationProvider = ({ children }: { children: React.ReactNode }) => 
       const token = await loginUserRequest(form);
       setBearer(token);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }, []);
 
+  const logout = useCallback(async () => {
+    try {
+      await SecureStore.deleteItemAsync("rememberedEmail");
+      await SecureStore.deleteItemAsync("password");
+      router.dismissAll();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
-    <AuthenticationContext.Provider value={{ register, login }}>
+    <AuthenticationContext.Provider value={{ register, login, logout }}>
       {children}
     </AuthenticationContext.Provider>
   );
-}
+};
 
 export const useAuthentication = () => {
   const context = useContext(AuthenticationContext);
