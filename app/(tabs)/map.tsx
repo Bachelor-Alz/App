a62be84a6cea4a5586e6ed3f6a9ac072cf8abb8e
Andreas Mertz <Andreasmertz11@gmail.com>
2025-04-16 23:16:46 +0200

    Ready for map integration

---
+++ app/(tabs)/map.tsx
@@ -0,0 +1,30 @@
+import { View, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+
+const Map = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 1,
+          longitudeDelta: 1,
+        }}
+      />
+    </View>
+  );
+};
+
+export default Map;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
