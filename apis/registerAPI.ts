d021688063c99695b242372786ddf4c6d77e496a
Andreas Mertz <Andreasmertz11@gmail.com>
2025-04-29 21:11:10 +0200

    Fixed infinite loop and being unable to register

--- apis/registerAPI.ts
+++ apis/registerAPI.ts
@@ -20,6 +20,7 @@ type CreateUserResponseProps = {
 
 export const createUserRequest = (userData: CreateUserRequestProps): Promise<CreateUserResponseProps> => {
   const { address, confirmPassword, ...rest } = userData;
+  console.log(userData);
   return axiosInstance
     .post(`/api/User/register`, rest)
     .then((res) => res.data)
